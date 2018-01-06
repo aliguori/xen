2579c1cafe8c4151994b81059c9ad3b17e899564
Anthony Liguori <aliguori@amazon.com>
2018-01-06 10:00:48 -0800

    vixen: introduce is_vixen() to allow altering behavior
    Vixen (Virtualized Xen) is a paravirtual mode of Xen where
    paravirtual I/O is passed through from the parent hypervisor
    all the way through the dom0 guest.  The dom0 guest is also
    deprivileged and renumbered to give the appearance that it
    is running as a normal PV guest.
    
    Signed-off-by: Anthony Liguori <aliguori@amazon.com>
    ---
    v1 -> v2
     - ARM stubs

---
+++ xen/include/asm-arm/guest/vixen.h
@@ -0,0 +1,81 @@
+/******************************************************************************
+ * include/asm-x86/guest.h
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms and conditions of the GNU General Public
+ * License, version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2018 Amazon.com, Inc. or its affiliates.
+ */
+
+#ifndef __ARM_GUEST_VIXEN_H__
+#define __ARM_GUEST_VIXEN_H__
+
+#include <xen/types.h>
+
+static inline int
+HYPERVISOR_xen_version(int cmd, void *arg)
+{
+    return -ENOSYS;
+}
+
+static inline unsigned long
+HYPERVISOR_hvm_op(int op, void *arg)
+{
+    return -ENOSYS;
+}
+
+static inline int
+HYPERVISOR_grant_table_op(unsigned int cmd, void *uop, unsigned int count)
+{
+    return -ENOSYS;
+}
+
+static inline long
+HYPERVISOR_memory_op(unsigned int cmd, void *arg)
+{
+    return -ENOSYS;
+}
+
+static inline int
+HYPERVISOR_event_channel_op(int cmd, void *arg)
+{
+    return -ENOSYS;
+}
+
+static inline int
+HYPERVISOR_sched_op(int cmd, void *arg)
+{
+    return -ENOSYS;
+}
+
+static inline int
+HYPERVISOR_vcpu_op(int cmd, int vcpuid, void *extra_args)
+{
+    return -ENOSYS;
+}
+
+static inline bool is_vixen(void)
+{
+    return false;
+}
+
+static inline bool vixen_has_per_cpu_notifications(void)
+{
+    return false;
+}
+
+static inline bool vixen_ring_process(uint16_t port)
+{
+    return false;
+}
+
+#endif
